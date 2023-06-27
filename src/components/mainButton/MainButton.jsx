b422484c58ba6b704f4419ebae242fadf72a7bb7
YEFFAZzI <42146349+YEFFAZzI@users.noreply.github.com>
2023-06-27 13:01:30 +0000

    first build

--- src/components/mainButton/MainButton.jsx
+++ src/components/mainButton/MainButton.jsx
@@ -4,7 +4,7 @@ import './MainButton.css';
 function MainButton(props) {
   return (
     <div className="mainBtn">
-        <img src={props.logo}  alt="logo" />
+        <img src={props.logo}  alt="logo" className='imglogo'/>
         <div>{props.title} </div>
     </div>
   );
