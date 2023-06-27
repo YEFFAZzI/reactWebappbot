3d09654bcb7d30f5d6170a791ba75e0860187d4c
YEFFAZzI <42146349+YEFFAZzI@users.noreply.github.com>
2023-06-27 11:22:47 +0000

    sdfs

--- src/components/mainButton/MainButton.jsx
+++ src/components/mainButton/MainButton.jsx
@@ -3,7 +3,7 @@ import './MainButton.css';
 
 function MainButton(props) {
   return (
-    <div className='mainBtn'>
+    <div className="mainBtn">
         <div></div>
         <div>{props.title} </div>
     </div>
